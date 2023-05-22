fd1507a34f98d2468c88e7e4e236ff30e5611803
denizyuk <yu.deniz@gmail.com>
2023-05-22 21:44:49 +0200

    added modals component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
 import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 export const metadata = {
   title: 'Fakebnb',
@@ -22,7 +23,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-        <Navbar />
+          <Modal isOpen />
+          <Navbar />
         </ClientOnly>
         {children}
         </body>
